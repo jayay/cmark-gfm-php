1c4dd8ab6b6bd89f67bfd03ec7c9ee4b7b6165f3
jayay <meine-adresse@live.de>
2020-06-19 22:13:17 +0200

    Cleanup

--- src/MarkdownParser.php
+++ src/MarkdownParser.php
@@ -11,12 +11,14 @@ class MarkdownParser {
         $this->ffi = FFI::scope("CMARKGFM");
     }
 
+    /**
+     * @param string $input
+     * @param int $options
+     * @return string
+     */
     public function parse_string(string $input, int $options = 0): string {
         $mem_allocator = $this->ffi->cmark_get_arena_mem_allocator();
-        $c_int = FFI::new('int');
-        $c_int->cdata = $options;
         $parser = $this->ffi->cmark_parser_new_with_mem($options, $mem_allocator);
-        
         $this->ffi->cmark_parser_feed($parser, $input, strlen($input));
 
         $document = $this->ffi->cmark_parser_finish($parser);
@@ -24,10 +26,8 @@ class MarkdownParser {
         $result = $this->ffi->cmark_render_html_with_mem
             ($document, $options, NULL, $mem_allocator);
         $result_php = FFI::string($result);
-    
-        // $this->ffi->$mem_allocator->free($result);
         $this->ffi->cmark_arena_reset();
-        // $this->ffi->cmark_release_plugins();
+
         return $result_php;
     }
 }
