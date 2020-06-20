f0347ae2b3e7dbb22bfb27b7813e2ce0ec1ccd24
jayay <meine-adresse@live.de>
2020-06-20 09:09:55 +0200

    Add PHPStan

--- tests/SmokeTest.php
+++ tests/SmokeTest.php
@@ -3,10 +3,10 @@
 class SmokeTest
    extends PHPUnit\Framework\TestCase
 {
-    public function testRender()
+    public function testRender(): void
     {
         $pInstance = new \jayay\CommonMarkGithub\MarkdownParser();
-        $result = $pInstance->parse_string(file_get_contents(__DIR__.'/resources/SmokeTest.input.md'));
+        $result = $pInstance->parse_string(file_get_contents(__DIR__.'/resources/SmokeTest.input.md') ?: '');
         $this->assertStringEqualsFile(__DIR__.'/resources/SmokeTest.output.html', $result);
     }
 }
